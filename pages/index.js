7db2509e551d245bd752558e6392907c53e00283
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-24 00:04:59 -0400

    [FEAT] Create main layout

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styled from '@emotion/styled'
-
-const Heading = styled.h1`
-  color: red;
-`;
+import Layout from '../components/layout/Layout'
 
 export default function Home() {
   return (
     <div>
-      <Heading>Start</Heading>
+      <Layout>
+        <h1>Start</h1>
+      </Layout>
     </div>
   )
 }
