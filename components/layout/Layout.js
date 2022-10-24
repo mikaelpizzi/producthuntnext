b9423d41b46bdbcd83f9aee8a77061c964847de8
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-24 19:22:48 -0400

    [ADD] Add Google fonts and static files

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import Header from './Header';
-import { Global, css } from '@emotion/react'
+import { Global, css } from '@emotion/react';
+import Head from 'next/head';
 
 const Layout = props => {
     return (  
@@ -25,11 +26,22 @@ const Layout = props => {
                     body {
                         font-size: 1.6rem;
                         line-height: 1.5;
+                        font-family: 'PT Sans', sans-serif;
                     }
 
                     h1, h2, h3 {
                         margin: 0 0 2rem 0;
                         line-height: 1.5;
+                    }
+
+                    h1, h2 {
+                        font-family: 'Roboto Slab', serif;
+                        font-weight: 700;
+                    }
+
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
+                        font-weight: 700;
                     }
 
                     ul {
@@ -43,6 +55,17 @@ const Layout = props => {
                     }
                 `}
             />
+
+            <Head>
+                <html lang='en' />
+                <title>Product Hunt Firebase & Next.js</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet" />
+                <link href='/static/css/app.css' rel='stylesheet' />
+            </Head>
+
             <Header />
 
             <main>
