9d2a8a26347b0e38001ea35554cfd10e9cb92c60
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-24 22:29:40 -0400

    [FEAT] Creat form to create account

--- pages/create-account.js
+++ pages/create-account.js
@@ -5,6 +5,42 @@ export default function CreateAccount() {
     <div>
       <Layout>
         <h1>Create Account</h1>
+        <form>
+            <div>
+                <label htmlFor='name'>Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    placeholder="Your Name"
+                    name="name"
+                />
+            </div>
+
+            <div>
+                <label htmlFor='email'>Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Your email"
+                    name="email"
+                />
+            </div>
+
+            <div>
+                <label htmlFor='password'>Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Your Password"
+                    name="password"
+                />
+            </div>
+
+            <input
+                type="submit"
+                value="Create Account"
+            />
+        </form>
       </Layout>
     </div>
   )
