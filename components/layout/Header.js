eafa50843164807a0ec0726e4e48041535c7bf97
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-24 20:33:56 -0400

    [ADD] Add css to header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,13 +1,41 @@
 import Link from "next/link";
 import Search from "../ui/Search";
 import Navigation from "./Navigation";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+const HeaderContainer = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width: 768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`
+
+const Logo = styled.p`
+    color: var(--orange);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`
 
 const Header = () => {
     return (  
-        <header>
-            <div>
+        <header
+            css={css`
+                border-bottom: 2px solid var(--gray3);
+                padding: 1rem 0;
+            `}
+        >
+            <HeaderContainer>
                 <div>
-                    <p>P</p>
+                    <Link href='/'>
+                        <Logo>P</Logo>
+                    </Link>
 
                     <Search />
 
@@ -22,7 +50,7 @@ const Header = () => {
                     <Link href="/">Log in</Link>
                     <Link href="/">Sign up</Link>
                 </div>
-            </div>
+            </HeaderContainer>
         </header>
     );
 }
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -11,6 +11,7 @@ const Layout = props => {
                     :root {
                         --gray: #3d3d3d;
                         --gray2: #6f6f6f;
+                        --gray3: #e1e1e1;
                         --orange: #da552f;
                     }
 
