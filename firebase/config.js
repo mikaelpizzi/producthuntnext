7d7fb679dd1aaf85bc192c8d859474cc6c974b3b
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-25 23:38:25 -0400

    [ADD] Add Firebase to project

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA_9rPZreTsPVkMqACeJDwZCjHO03xulwA",
+    authDomain: "product-hunt-3f790.firebaseapp.com",
+    projectId: "product-hunt-3f790",
+    storageBucket: "product-hunt-3f790.appspot.com",
+    messagingSenderId: "416660676010",
+    appId: "1:416660676010:web:818591fd954f983df21fd3"
+};
+
+export default firebaseConfig;
