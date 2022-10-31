02d2d714642afc25078737e3986556c742c72019
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-30 21:38:25 -0400

    [FEAT] Show info from databse

--- components/layout/ProductDetails.js
+++ components/layout/ProductDetails.js
@@ -1,21 +1,38 @@
+import styled from "@emotion/styled";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
+const Image = styled.img`
+    width: 200px;
+`;
+
 const ProductDetails = ({ product }) => {
 
-    const { id, comments, created, description, company, name, url, urlimage, votes } = product;
+    const { id, comments, created, description, company, name, url, imageurl, votes } = product;
 
     return (  
         <li>
             <div>
                 <div>
-
+                    <Image src={imageurl} />
                 </div>
 
                 <div>
                     <h1>{name}</h1>
+
+                    <p>{description}</p>
+
+                    <div>
+                        <img src="/static/img/comment.png" />
+                        <p>{comments.length} comments</p>
+
+                        <p>Posted {formatDistanceToNow(new Date(created))} ago</p>
+                    </div>
                 </div>
             </div>
 
             <div>
-
+                <div>&#9650;</div>
+                <p>{votes}</p>
             </div>
         </li>
     );
