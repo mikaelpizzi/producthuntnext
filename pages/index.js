ac04944bd6d556e379ef33792fe3064ab4e726aa
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-30 21:02:00 -0400

    [FEAT] Consult Firebase database

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,31 @@
+import { useContext, useEffect, useState } from 'react'
 import Layout from '../components/layout/Layout'
+import { FirebaseContext } from '../firebase';
 
 export default function Home() {
+
+  const [ products, setProducts ] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const getProducts = () => {
+      firebase.db.collection('products').orderBy('created', 'desc').onSnapshot(handleSnapshot);
+    }
+    getProducts();
+  }, []);
+
+  function handleSnapshot(snapshot) {
+    const products = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+
+    setProducts(products);
+  }
+
   return (
     <div>
       <Layout>
