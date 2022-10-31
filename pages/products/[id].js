8fbfd1dec4f08afed339efca8cb8750a10e53021
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-30 23:45:42 -0400

    [ADD] Add product html structure

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,8 +1,19 @@
+import React from 'react';
 import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 import { FirebaseContext } from "../../firebase";
 import Error404 from "../../components/layout/404";
 import Layout from "../../components/layout/Layout";
+import { css } from "@emotion/react";
+import styled from '@emotion/styled';
+
+const ProductContainer = styled.div`
+    @media (min-width: 768px) {
+        display: grid;
+        grid-template-columns: 2fr 1fr;
+        column-gap: 2rem;
+    }
+`;
 
 const Product = () => {
     // Component state
@@ -33,9 +44,35 @@ const Product = () => {
         }
     }, [id])
 
+    if (Object.keys(product).length === 0) return 'Loading...';
+
+    const { comments, created, description, company, name, url, imageurl, votes } = product;
+
+
     return (  
         <Layout>
-            { error && <Error404 /> }
+            <>
+                { error && <Error404 /> }
+                
+                <div className="contenedor">
+                    <h1
+                        css={css`
+                            text-align: center;
+                            margin-top: 5rem;
+                        `}
+                    ></h1>
+
+                    <ProductContainer>
+                        <div>
+                            1
+                        </div>
+
+                        <aside>
+                            2
+                        </aside>
+                    </ProductContainer>
+                </div>
+            </>
         </Layout>
     );
 }
