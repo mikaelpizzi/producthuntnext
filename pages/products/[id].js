d6b896f75dd758940938984858ceac1256304df3
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-30 23:00:14 -0400

    [FEAT] Get product id from router

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,6 +1,18 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 const Product = () => {
+    // Routing for getting actual id
+    const router = useRouter();
+    const { query: {id}} = router;
+
+    useEffect(() => {
+        if (id) {
+            console.log('There is something!');
+        }
+    }, [id])
     return (  
-        <h1>From [id].js</h1>
+        <h1>From {id}</h1>
     );
 }
  
