6e3280292eb3b12b3f88dde34c5caf2f9fe48913
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-11-01 01:20:47 -0400

    [FEAT] Create popular page and implement hook

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,11 @@
-import { useContext, useEffect, useState } from 'react'
 import Layout from '../components/layout/Layout'
 import ProductDetails from '../components/layout/ProductDetails';
-import { FirebaseContext } from '../firebase';
+import useProducts from '../hooks/useProducts';
 
 export default function Home() {
 
-  const [ products, setProducts ] = useState([]);
-
-  const { firebase } = useContext(FirebaseContext);
-
-  useEffect(() => {
-    const getProducts = () => {
-      firebase.db.collection('products').orderBy('created', 'desc').onSnapshot(handleSnapshot);
-    }
-    getProducts();
-  }, []);
-
-  function handleSnapshot(snapshot) {
-    const products = snapshot.docs.map(doc => {
-      return {
-        id: doc.id,
-        ...doc.data()
-      }
-    });
-
-    setProducts(products);
-  }
-
+  const { products } = useProducts('created');
+  
   return (
     <div>
       <Layout>
