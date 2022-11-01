01e61a71b61b91f9d3c76b585dbfff72cc1417d6
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-11-01 18:56:44 -0400

    [ADD] Add Firebase config for deploy

--- firebase/config.js
+++ firebase/config.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
     projectId: "product-hunt-3f790",
     storageBucket: "product-hunt-3f790.appspot.com",
     messagingSenderId: "416660676010",
-    appId: "1:416660676010:web:818591fd954f983df21fd3"
+    appId: "1:416660676010:web:bd03bc1185f1b256f21fd3"
 };
 
 export default firebaseConfig;
