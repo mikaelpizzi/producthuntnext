b0f1e65198ae86043cf18e0b3519fddcd78952f7
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-11-01 00:19:13 -0400

    [IMP] Refactor code with ternary

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -33,6 +33,7 @@ const Product = () => {
     const [ product, setProduct ] = useState({});
     const [ error, setError ] = useState(false);
     const [ comment, saveComment ] = useState({});
+    const [ consultDB, setConsultDB ] = useState(true);
 
     // Routing for getting actual product id
     const router = useRouter();
@@ -42,23 +43,25 @@ const Product = () => {
     const { firebase, user } = useContext(FirebaseContext);
 
     useEffect(() => {
-        if (id) {
+        if (id && consultDB) {
             const getProduct = async () => {
                 const productQuery = await firebase.db.collection('products').doc(id);
                 const product = await productQuery.get();
 
                 if (product.exists) {
                     setProduct( product.data() ); 
+                    setConsultDB(false);
                 } else {
                     setError(true);
+                    setConsultDB(false);
                 }
 
             }
             getProduct();
         }
-    }, [id, product])
+    }, [id])
 
-    if (Object.keys(product).length === 0) return 'Loading...';
+    if (Object.keys(product).length === 0 && !error) return 'Loading...';
 
     const { comments, created, description, company, name, url, imageurl, votes, creator, hasVoted } = product;
 
@@ -88,6 +91,8 @@ const Product = () => {
             ...product,
             votes: newTotal
         })
+
+        setConsultDB(true); // When voting, then consult db
     }
 
     // Functions to create comments
@@ -131,110 +136,116 @@ const Product = () => {
             ...product,
             comments: newComments
         })
+
+        setConsultDB(true); // When commenting, then consult db
     }
 
     return (  
         <Layout>
             <>
-                { error && <Error404 /> }
-                
-                <div className="contenedor">
-                    <h1
-                        css={css`
-                            text-align: center;
-                            margin-top: 5rem;
-                        `}
-                    ></h1>
-
-                    <ProductContainer>
-                        <div>
-                            <p>Posted {formatDistanceToNow(new Date(created))} ago</p>
-                            <p>By: <strong>{creator.name}</strong> of {company}</p>
-
-                            <img src={imageurl} />
-                            <p>{description}</p>
-
-                            { user && (
-                                <>
-                                    <h2>Add a comment</h2>
-                                    <form
-                                        onSubmit={addComment}
-                                    >
-                                        <Field>
-                                            <input 
-                                                type="text"
-                                                name="message"
-                                                onChange={onChangeComment}
-                                            />
-                                        </Field>
-
-                                        <InputSubmit
-                                            type="submit"
-                                            value="Add comment"
-                                        />
-                                    </form>
-                                </>
-                            ) }
-
-                            <h2
+                { error ? <Error404 /> : (
+                    <>
+                        <div className="contenedor">
+                            <h1
                                 css={css`
-                                    margin: 2rem 0;
-                                `}
-                            >Comments</h2>
-
-                            { comments.length === 0 ? "No comments yet" : (
-                                <ul>
-                                { comments.map((comment, i) => (
-                                    <li
-                                        key={`${comment.userId}-${i}`}
-                                        css={css`
-                                            border: 1px solid #e1e1e1;
-                                            padding: 2rem;
-                                        `}
-                                    >
-                                        <p>{comment.message}</p>
-                                        <p>
-                                            Written by: 
-                                            <span
-                                                css={css`
-                                                    font-weight: bold;
-                                                `}
-                                            >{' '}{comment.userName}</span>
-                                        </p>
-                                        { isCreator(comment.userId) && <ProductCreator>Is creator</ProductCreator> }
-                                    </li>
-                                ))}
-                                </ul>
-                            )}
-                            
-                        </div>
-
-                        <aside>
-                            <Button
-                                target="_blank"
-                                bgColor="#da552f"
-                                href={url}
-                            >Visit URL</Button>
-
-                            <div
-                                css={css`
+                                    text-align: center;
                                     margin-top: 5rem;
                                 `}
-                            >
-                                <p
-                                    css={css`
-                                        text-align: center;
-                                    `}
-                                >{votes} Upvote</p>
-                                
-                                { user && 
-                                <Button
-                                    onClick={upvoteProduct}
-                                >Vote</Button> }
-                            </div>
-                        </aside>
-                    </ProductContainer>
-                </div>
+                            >{name}</h1>
+
+                            <ProductContainer>
+                                <div>
+                                    <p>Posted {formatDistanceToNow(new Date(created))} ago</p>
+                                    <p>By: <strong>{creator.name}</strong> of {company}</p>
+
+                                    <img src={imageurl} />
+                                    <p>{description}</p>
+
+                                    { user && (
+                                        <>
+                                            <h2>Add a comment</h2>
+                                            <form
+                                                onSubmit={addComment}
+                                            >
+                                                <Field>
+                                                    <input 
+                                                        type="text"
+                                                        name="message"
+                                                        onChange={onChangeComment}
+                                                    />
+                                                </Field>
+
+                                                <InputSubmit
+                                                    type="submit"
+                                                    value="Add comment"
+                                                />
+                                            </form>
+                                        </>
+                                    ) }
+
+                                    <h2
+                                        css={css`
+                                            margin: 2rem 0;
+                                        `}
+                                    >Comments</h2>
+
+                                    { comments.length === 0 ? "No comments yet" : (
+                                        <ul>
+                                        { comments.map((comment, i) => (
+                                            <li
+                                                key={`${comment.userId}-${i}`}
+                                                css={css`
+                                                    border: 1px solid #e1e1e1;
+                                                    padding: 2rem;
+                                                `}
+                                            >
+                                                <p>{comment.message}</p>
+                                                <p>
+                                                    Written by: 
+                                                    <span
+                                                        css={css`
+                                                            font-weight: bold;
+                                                        `}
+                                                    >{' '}{comment.userName}</span>
+                                                </p>
+                                                { isCreator(comment.userId) && <ProductCreator>Creator</ProductCreator> }
+                                            </li>
+                                        ))}
+                                        </ul>
+                                    )}
+                                    
+                                </div>
+
+                                <aside>
+                                    <Button
+                                        target="_blank"
+                                        bgColor="#da552f"
+                                        href={url}
+                                    >Visit URL</Button>
+
+                                    <div
+                                        css={css`
+                                            margin-top: 5rem;
+                                        `}
+                                    >
+                                        <p
+                                            css={css`
+                                                text-align: center;
+                                            `}
+                                        >{votes} Upvote</p>
+                                        
+                                        { user && 
+                                        <Button
+                                            onClick={upvoteProduct}
+                                        >Vote</Button> }
+                                    </div>
+                                </aside>
+                            </ProductContainer>
+                        </div>
+                    </>
+                ) }
+                
+
             </>
         </Layout>
     );
