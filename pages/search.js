41d1a502f8b5b1241c8cc805281f4b04e24df806
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-11-01 01:48:58 -0400

    [FEAT] Filter products

--- pages/search.js
+++ pages/search.js
@@ -1,18 +1,46 @@
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react';
 import Layout from '../components/layout/Layout'
+import useProducts from '../hooks/useProducts';
 
 export default function Search() {
 
   const router = useRouter();
-  
   const { query: { q }} = router;
   
-  console.log(q);
+  // All the products
+  const { products } = useProducts('created');
+  const [ result, setResult ] = useState([]);
+
+  useEffect(() => {
+    const query = q.toLowerCase();
+
+    const filter = products.filter(product => {
+      return (
+        product.name.toLowerCase().includes(query) ||
+        product.description.toLowerCase().includes(query) ||
+        product.creator.name.toLowerCase().includes(query)
+      )
+    });
+
+    setResult(filter);
+  }, [q, products]);
 
   return (
     <div>
       <Layout>
-        <h1>Search</h1>
+        <div className='listado-productos'>
+          <div className='contenedor'>
+            <ul className='bg-white'>
+              { result.map(product => (
+                <ProductDetails 
+                  key={product.id}
+                  product={product}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   )
